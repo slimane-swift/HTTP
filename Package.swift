5dd210e8f9dc4c84f54ee4a36d6a59c23cd27dad
noppoman <yuki@miketokyo.com>
2016-08-12 14:11:20 +0900

    use open-swift/S4

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/slimane-swift/MediaType.git", majorVersion: 0, minor: 12),
         .Package(url: "https://github.com/slimane-swift/URI.git", majorVersion: 0, minor: 12),
-        .Package(url: "https://github.com/noppoMan/S4.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 12),
     ]
 )
